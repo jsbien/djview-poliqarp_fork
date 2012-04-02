34b90db7210222d84a77664870484c541df272a9
Michal Rudolf <michal@rudolf.waw.pl>
2012-04-02 09:21:09 +0200

    Fix clicking on graphical items.

--- src/djvuitemlist.cpp
+++ src/djvuitemlist.cpp
@@ -7,95 +7,94 @@
 #include "qdjvuhttp.h"
 
 DjVuItemList::DjVuItemList(QWidget *parent) :
-    QWidget(parent)
+	 QWidget(parent)
 {
-    setLayout(m_layout = new QGridLayout);
-    m_context = new QDjVuContext("Demo", this);
-    m_currentItem = -1;
+	 setLayout(m_layout = new QGridLayout);
+	 m_context = new QDjVuContext("Demo", this);
+	 m_currentItem = -1;
 }
 
 void DjVuItemList::clear()
 {
-    for (int i = 0; i < m_items.count(); i++) {
-        layout()->removeWidget(m_items[i].label);
-        layout()->removeWidget(m_items[i].label);
-        m_items[i].label->deleteLater();
-        m_items[i].djvu->setDocument(0);
-        m_items[i].djvu->deleteLater();
-        m_items[i].document->deleteLater();
-    }
-    m_items.clear();
-    m_currentItem = -1;
+	 for (int i = 0; i < m_items.count(); i++) {
+		  layout()->removeWidget(m_items[i].label);
+		  layout()->removeWidget(m_items[i].label);
+		  m_items[i].label->deleteLater();
+		  m_items[i].djvu->setDocument(0);
+		  m_items[i].djvu->deleteLater();
+		  m_items[i].document->deleteLater();
+	 }
+	 m_items.clear();
+	 m_currentItem = -1;
 }
 
 void DjVuItemList::addItem(const DjVuLink& link)
 {
-    int row = m_items.count();
+	 int row = m_items.count();
 
-    DjVuItem item;
-    item.label = new QLabel(QString::number(row + 1));
+	 DjVuItem item;
+	 item.label = new QLabel(QString::number(row + 1));
 
-    item.djvu = new QDjVuWidget(this);
-    item.djvu->setMaximumHeight(40);
-    item.djvu->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    item.djvu->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    item.djvu->viewport()->installEventFilter(this);
+	 item.djvu = new QDjVuWidget(this);
+	 item.djvu->setMaximumHeight(40);
+	 item.djvu->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	 item.djvu->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	 item.djvu->viewport()->installEventFilter(this);
 
-    m_layout->addWidget(item.label, row, 0);
-    m_layout->addWidget(item.djvu, row, 1);
+	 m_layout->addWidget(item.label, row, 0);
+	 m_layout->addWidget(item.djvu, row, 1);
 
-    item.document = new QDjVuHttpDocument(this);
-    if (item.document->setUrl(m_context, link.link()))
-        item.djvu->setDocument(item.document);
-    connect(item.document, SIGNAL(docinfo()), this, SLOT(documentLoaded()));
+	 item.document = new QDjVuHttpDocument(this);
+	 if (item.document->setUrl(m_context, link.link()))
+		  item.djvu->setDocument(item.document);
+	 connect(item.document, SIGNAL(docinfo()), this, SLOT(documentLoaded()));
 
-    item.link = link;
+	 item.link = link;
 
-    m_items.append(item);
+	 m_items.append(item);
 
 }
 
 void DjVuItemList::documentLoaded()
 {
-    QDjVuDocument* document = dynamic_cast<QDjVuDocument*>(sender());
-    for (int i = 0; i < m_items.count(); i++)
-        if (m_items[i].document == document) {
-            showDocument(i);
-            break;
-        }
+	 QDjVuDocument* document = dynamic_cast<QDjVuDocument*>(sender());
+	 for (int i = 0; i < m_items.count(); i++)
+		  if (m_items[i].document == document) {
+				showDocument(i);
+				break;
+		  }
 }
 
 void DjVuItemList::showDocument(int index)
 {
-    DjVuLink link = m_items[index].link;
-    QDjVuWidget::Position pos;
-    pos.pageNo = link.page();
-    pos.inPage = true;
-    pos.posPage = link.highlighted().topLeft();
+	 DjVuLink link = m_items[index].link;
+	 QDjVuWidget::Position pos;
+	 pos.pageNo = link.page();
+	 pos.inPage = true;
+	 pos.posPage = link.highlighted().topLeft();
 
-    m_items[index].djvu->setPosition(pos, QPoint(m_items[index].djvu->width() / 2,
-                                                 m_items[index].djvu->height() / 2));
+	 m_items[index].djvu->setPosition(pos, QPoint(m_items[index].djvu->width() / 2,
+																 m_items[index].djvu->height() / 2));
 
-    QSettings settings;
-    QColor color(settings.value("Display/highlight", "#ffff00").toString());
-    color.setAlpha(96);
-    m_items[index].djvu->addHighlight(link.page(),
-                                      link.highlighted().left(),
-                                      link.highlighted().top(),
-                                      link.highlighted().width(),
-                                      link.highlighted().height(), color);
+	 QSettings settings;
+	 QColor color(settings.value("Display/highlight", "#ffff00").toString());
+	 color.setAlpha(96);
+	 m_items[index].djvu->addHighlight(link.page(),
+												  link.highlighted().left(),
+												  link.highlighted().top(),
+												  link.highlighted().width(),
+												  link.highlighted().height(), color);
 }
 
 
 bool DjVuItemList::eventFilter(QObject *widget, QEvent *event)
 {
-    for (int i = 0; i < m_items.count(); i++)
-        if (m_items[i].djvu == widget) {
-            if (i != m_currentItem && event->type() ==
-                    QEvent::MouseButtonPress)
-                emit documentRequested(m_items[m_currentItem = i].link);
-            break;
-        }
-    return false;
+	if (event->type() != QEvent::MouseButtonPress)
+		return false;
+
+	for (int i = 0; i < m_items.count(); i++)
+		if (m_items[i].djvu->viewport() == widget && i != m_currentItem)
+			emit documentRequested(m_items[m_currentItem = i].link);
+	 return false;
 }
 
--- src/poliqarpwidget.cpp
+++ src/poliqarpwidget.cpp
@@ -9,215 +9,214 @@
 
 
 PoliqarpWidget::PoliqarpWidget(QWidget *parent) :
-    QWidget(parent)
+	 QWidget(parent)
 {
-    ui.setupUi(this);
-    ui.textResultTable->verticalHeader()->setDefaultSectionSize(
-                1.2 * ui.textResultTable->verticalHeader()->fontMetrics().height());
+	 ui.setupUi(this);
+	 ui.textResultTable->verticalHeader()->setDefaultSectionSize(
+					 1.2 * ui.textResultTable->verticalHeader()->fontMetrics().height());
 
-    connect(ui.connectButton, SIGNAL(clicked()), this, SLOT(connectToServer()));
-    connect(ui.searchButton, SIGNAL(clicked()), this, SLOT(doSearch()));
-    connect(ui.corpusCombo, SIGNAL(currentIndexChanged(int)), this,
-              SLOT(doSelectSource()));
-    connect(ui.textResultTable, SIGNAL(doubleClicked(QModelIndex)), this,
-              SLOT(showDocument(QModelIndex)));
-    connect(ui.graphicalResultList, SIGNAL(documentRequested(DjVuLink)), this,
-              SIGNAL(documentRequested(DjVuLink)));
-    connect(ui.queryCombo->lineEdit(), SIGNAL(returnPressed()), this,
-              SLOT(doSearch()));
-    connect(ui.resultWidget, SIGNAL(currentChanged(int)), this,
-            SLOT(displayModeChanged()));
+	 connect(ui.connectButton, SIGNAL(clicked()), this, SLOT(connectToServer()));
+	 connect(ui.searchButton, SIGNAL(clicked()), this, SLOT(doSearch()));
+	 connect(ui.corpusCombo, SIGNAL(currentIndexChanged(int)), this,
+				  SLOT(doSelectSource()));
+	 connect(ui.textResultTable, SIGNAL(doubleClicked(QModelIndex)), this,
+				  SLOT(showDocument(QModelIndex)));
+	 connect(ui.graphicalResultList, SIGNAL(documentRequested(DjVuLink)), this,
+				  SIGNAL(documentRequested(DjVuLink)));
+	 connect(ui.queryCombo->lineEdit(), SIGNAL(returnPressed()), this,
+				  SLOT(doSearch()));
+	 connect(ui.resultWidget, SIGNAL(currentChanged(int)), this,
+				SLOT(displayModeChanged()));
 
-    m_poliqarp = new Poliqarp(this);
-    connect(m_poliqarp, SIGNAL(connected(QStringList)), this,
-              SLOT(connected(QStringList)));
-    connect(m_poliqarp, SIGNAL(connectionError(QString)), this,
-              SLOT(connectionError(QString)));
-    connect(m_poliqarp, SIGNAL(sourceSelected()), this,
-              SLOT(sourceSelected()));
-    connect(m_poliqarp, SIGNAL(queryFinished()), this,
-              SLOT(updateQueries()));
+	 m_poliqarp = new Poliqarp(this);
+	 connect(m_poliqarp, SIGNAL(connected(QStringList)), this,
+				  SLOT(connected(QStringList)));
+	 connect(m_poliqarp, SIGNAL(connectionError(QString)), this,
+				  SLOT(connectionError(QString)));
+	 connect(m_poliqarp, SIGNAL(sourceSelected()), this,
+				  SLOT(sourceSelected()));
+	 connect(m_poliqarp, SIGNAL(queryFinished()), this,
+				  SLOT(updateQueries()));
 
-    connect(ui.moreButton, SIGNAL(clicked()), m_poliqarp, SLOT(fetchMore()));
+	 connect(ui.moreButton, SIGNAL(clicked()), m_poliqarp, SLOT(fetchMore()));
 
-    QSettings settings;
-    settings.beginGroup("Poliqarp");
-    ui.queryCombo->addItems(settings.value("queries").toStringList());
-    ui.queryCombo->clearEditText();
-    settings.endGroup();
+	 QSettings settings;
+	 settings.beginGroup("Poliqarp");
+	 ui.queryCombo->addItems(settings.value("queries").toStringList());
+	 ui.queryCombo->clearEditText();
+	 settings.endGroup();
 
 }
 
 
 PoliqarpWidget::~PoliqarpWidget()
 {
-    QSettings settings;
-    settings.beginGroup("Poliqarp");
-    QStringList items;
-    for (int i = 0; i < ui.queryCombo->count(); i++)
-        items.append(ui.queryCombo->itemText(i));
-    settings.setValue("queries", items);
-    settings.endGroup();
+	 QSettings settings;
+	 settings.beginGroup("Poliqarp");
+	 QStringList items;
+	 for (int i = 0; i < ui.queryCombo->count(); i++)
+		  items.append(ui.queryCombo->itemText(i));
+	 settings.setValue("queries", items);
+	 settings.endGroup();
 }
 
 void PoliqarpWidget::connectToServer()
 {
-    QUrl url;
-    url.setHost(ui.urlCombo->currentText());
+	 QUrl url;
+	 url.setHost(ui.urlCombo->currentText());
 
-    // MRTODO: remove when Kanji is ported
-    if (url.host().contains("wbl"))
-        url.setPath("/en");
-    url.setScheme("http");
-    if (!url.isValid()) {
-        MessageDialog::warning(tr("This URL is not valid"));
-        return;
-    }
-    ui.corpusCombo->clear();
-    ui.connectButton->setEnabled(false);
-    clearQueries();
-    setCursor(QCursor(Qt::WaitCursor));
-    m_poliqarp->connectToServer(url);
+	 // MRTODO: remove when Kanji is ported
+	 if (url.host().contains("wbl"))
+		  url.setPath("/en");
+	 url.setScheme("http");
+	 if (!url.isValid()) {
+		  MessageDialog::warning(tr("This URL is not valid"));
+		  return;
+	 }
+	 ui.corpusCombo->clear();
+	 ui.connectButton->setEnabled(false);
+	 clearQueries();
+	 setCursor(QCursor(Qt::WaitCursor));
+	 m_poliqarp->connectToServer(url);
 }
 
 void PoliqarpWidget::doSearch()
 {
-    clearQueries();
-    setCursor(QCursor(Qt::WaitCursor));
-    m_poliqarp->runQuery(ui.queryCombo->currentText());
+	 clearQueries();
+	 setCursor(QCursor(Qt::WaitCursor));
+	 m_poliqarp->runQuery(ui.queryCombo->currentText());
 }
 
 void PoliqarpWidget::doSelectSource()
 {
-    ui.searchButton->setEnabled(false);
-    clearQueries();
-    m_poliqarp->setCurrentSource(ui.corpusCombo->currentIndex());
+	 ui.searchButton->setEnabled(false);
+	 clearQueries();
+	 m_poliqarp->setCurrentSource(ui.corpusCombo->currentIndex());
 }
 
 void PoliqarpWidget::connected(const QStringList& sources)
 {
-    unsetCursor();
-    ui.corpusCombo->addItems(sources);
-    ui.connectButton->setEnabled(true);
-    ui.corpusCombo->setEnabled(true);
-    QSettings settings;
-    int lastIndex = settings.value(QString("Poliqarp/") +
-                                             m_poliqarp->serverUrl().host(), 0).toInt();
-    ui.corpusCombo->setCurrentIndex(lastIndex);
+	 unsetCursor();
+	 ui.corpusCombo->addItems(sources);
+	 ui.connectButton->setEnabled(true);
+	 ui.corpusCombo->setEnabled(true);
+	 QSettings settings;
+	 int lastIndex = settings.value(QString("Poliqarp/") +
+															m_poliqarp->serverUrl().host(), 0).toInt();
+	 ui.corpusCombo->setCurrentIndex(lastIndex);
 }
 
 void PoliqarpWidget::connectionError(const QString &message)
 {
-    unsetCursor();
-    ui.connectButton->setEnabled(true);
-    MessageDialog::warning(message);
+	 unsetCursor();
+	 ui.connectButton->setEnabled(true);
+	 MessageDialog::warning(message);
 }
 
 void PoliqarpWidget::sourceSelected()
 {
-    ui.searchButton->setEnabled(true);
-    ui.queryCombo->setFocus();
-    QSettings settings;
-    settings.setValue(QString("Poliqarp/") + m_poliqarp->serverUrl().host(),
-                            ui.corpusCombo->currentIndex());
+	 ui.searchButton->setEnabled(true);
+	 ui.queryCombo->setFocus();
+	 QSettings settings;
+	 settings.setValue(QString("Poliqarp/") + m_poliqarp->serverUrl().host(),
+									 ui.corpusCombo->currentIndex());
 }
 
 void PoliqarpWidget::updateQueries()
 {
-    unsetCursor();
-    updateTextQueries();
-    updateGraphicalQueries();
+	 unsetCursor();
+	 updateTextQueries();
+	 updateGraphicalQueries();
 
-    ui.moreButton->setEnabled(m_poliqarp->hasMore());
+	 ui.moreButton->setEnabled(m_poliqarp->hasMore());
 }
 
 void PoliqarpWidget::showDocument(const QModelIndex& index)
 {
-    QHeaderView* header = ui.textResultTable->horizontalHeader();
-    ui.textResultTable->resizeColumnToContents(1);
-    int sizeLeft = header->width() - header->sectionSize(1) - 20;
-    header->resizeSection(0, sizeLeft / 2 - 2);
-    header->resizeSection(2, sizeLeft / 2 - 2);
-    if (index.isValid()) {
-        DjVuLink item = m_poliqarp->query(index.row());
-        if (item.link().isValid())
-            emit documentRequested(item);
-    }
+	 QHeaderView* header = ui.textResultTable->horizontalHeader();
+	 ui.textResultTable->resizeColumnToContents(1);
+	 int sizeLeft = header->width() - header->sectionSize(1) - 20;
+	 header->resizeSection(0, sizeLeft / 2 - 2);
+	 header->resizeSection(2, sizeLeft / 2 - 2);
+	 if (index.isValid()) {
+		  DjVuLink item = m_poliqarp->query(index.row());
+		  if (item.link().isValid())
+				emit documentRequested(item);
+	 }
 }
 
 void PoliqarpWidget::displayModeChanged()
 {
-    if (ui.resultWidget->currentIndex() == 0)
-        adjustTextColumns();
+	 if (ui.resultWidget->currentIndex() == 0)
+		  adjustTextColumns();
 }
 
 void PoliqarpWidget::updateTextQueries()
 {
-    int oldCount = ui.textResultTable->rowCount();
-    ui.textResultTable->setRowCount(m_poliqarp->queryCount());
-    QFont boldFont = ui.textResultTable->font();
-    boldFont.setBold(true);
-    for (int i = oldCount; i < m_poliqarp->queryCount(); i++) {
-        DjVuLink item = m_poliqarp->query(i);
-        QTableWidgetItem* left = new QTableWidgetItem(item.leftContext());
-        left->setTextAlignment(Qt::AlignRight);
-        ui.textResultTable->setItem(i, 0, left);
+	 int oldCount = ui.textResultTable->rowCount();
+	 ui.textResultTable->setRowCount(m_poliqarp->queryCount());
+	 QFont boldFont = ui.textResultTable->font();
+	 boldFont.setBold(true);
+	 for (int i = oldCount; i < m_poliqarp->queryCount(); i++) {
+		  DjVuLink item = m_poliqarp->query(i);
+		  QTableWidgetItem* left = new QTableWidgetItem(item.leftContext());
+		  left->setTextAlignment(Qt::AlignRight);
+		  ui.textResultTable->setItem(i, 0, left);
 
-        QTableWidgetItem* center = new QTableWidgetItem(item.word());
-        center->setTextAlignment(Qt::AlignCenter);
-        center->setFont(boldFont);
-        center->setForeground(Qt::darkBlue);
-        ui.textResultTable->setItem(i, 1, center);
+		  QTableWidgetItem* center = new QTableWidgetItem(item.word());
+		  center->setTextAlignment(Qt::AlignCenter);
+		  center->setFont(boldFont);
+		  center->setForeground(Qt::darkBlue);
+		  ui.textResultTable->setItem(i, 1, center);
 
-        QTableWidgetItem* right = new QTableWidgetItem(item.rightContext());
-        right->setTextAlignment(Qt::AlignLeft);
-        ui.textResultTable->setItem(i, 2, right);
-    }
-    if (m_poliqarp->queryCount() == 0)
-        ui.matchLabel->setText(tr("No matches"));
-    else {
-        QString text = ui.queryCombo->currentText();
-        int old = ui.queryCombo->findText(text);
-        if (old != -1)
-            ui.queryCombo->removeItem(old);
-        else if (ui.queryCombo->count() >= 20)
-            ui.queryCombo->removeItem(ui.queryCombo->count() - 1);
-        ui.queryCombo->insertItem(0, text);
-        ui.queryCombo->setCurrentIndex(0);
-        ui.matchLabel->setText(tr("Matches %1 of %2")
-                                      .arg(m_poliqarp->queryCount())
-                                      .arg(m_poliqarp->matchesFound()));
-    }
+		  QTableWidgetItem* right = new QTableWidgetItem(item.rightContext());
+		  right->setTextAlignment(Qt::AlignLeft);
+		  ui.textResultTable->setItem(i, 2, right);
+	 }
+	 if (m_poliqarp->queryCount() == 0)
+		  ui.matchLabel->setText(tr("No matches"));
+	 else {
+		  QString text = ui.queryCombo->currentText();
+		  int old = ui.queryCombo->findText(text);
+		  if (old != -1)
+				ui.queryCombo->removeItem(old);
+		  else if (ui.queryCombo->count() >= 20)
+				ui.queryCombo->removeItem(ui.queryCombo->count() - 1);
+		  ui.queryCombo->insertItem(0, text);
+		  ui.queryCombo->setCurrentIndex(0);
+		  ui.matchLabel->setText(tr("Matches %1 of %2")
+												  .arg(m_poliqarp->queryCount())
+												  .arg(m_poliqarp->matchesFound()));
+	 }
 
-    if (ui.textResultTable->isVisible())
-        adjustTextColumns();
+	 if (ui.textResultTable->isVisible())
+		  adjustTextColumns();
 }
 
 void PoliqarpWidget::updateGraphicalQueries()
 {
-    // Graphical results
-    int oldCount = ui.graphicalResultList->count();
-    for (int i = oldCount; i < m_poliqarp->queryCount(); i++)
-        ui.graphicalResultList->addItem(m_poliqarp->query(i));
+	 // Graphical results
+	 int oldCount = ui.graphicalResultList->count();
+	 for (int i = oldCount; i < m_poliqarp->queryCount(); i++)
+		  ui.graphicalResultList->addItem(m_poliqarp->query(i));
 }
 
 void PoliqarpWidget::clearQueries()
 {
-    ui.textResultTable->setRowCount(0);
-    ui.graphicalResultList->clear();
-    ui.matchLabel->clear();
-    emit documentRequested(DjVuLink());
+	 ui.textResultTable->setRowCount(0);
+	 ui.graphicalResultList->clear();
+	 ui.matchLabel->clear();
+	 emit documentRequested(DjVuLink());
 }
 
 void PoliqarpWidget::adjustTextColumns()
 {
-    // Resize columns
-    QHeaderView* header = ui.textResultTable->horizontalHeader();
-    ui.textResultTable->resizeColumnToContents(1);
-    int sizeLeft = header->width() - header->sectionSize(1) - 20;
-    header->resizeSection(0, sizeLeft / 2 - 5);
-    header->resizeSection(2, sizeLeft / 2 - 5);
-    qDebug() << "SL" << sizeLeft << header->width() << header->sectionSize(1);
+	 // Resize columns
+	 QHeaderView* header = ui.textResultTable->horizontalHeader();
+	 ui.textResultTable->resizeColumnToContents(1);
+	 int sizeLeft = header->width() - header->sectionSize(1) - 20;
+	 header->resizeSection(0, sizeLeft / 2 - 5);
+	 header->resizeSection(2, sizeLeft / 2 - 5);
 }
 
 
